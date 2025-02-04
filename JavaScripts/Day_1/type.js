ea2d86cb33b15120b3c4cb64f767c611c4c67197
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-04 21:20:52 +0600

    Add initial variable declarations for strings, numbers, booleans, arrays, and objects in Day_1 JavaScript files

---
+++ JavaScripts/Day_1/type.js
@@ -0,0 +1,21 @@
+// Strings
+var name = "Shaun";
+let address = "123 Main St";
+
+
+// Numbers
+
+let age = 30;
+let height = 5.8;
+
+// Booleans
+
+let hasHobbies = true;
+
+// Arrays
+
+let hobbies = ['Sports', 'Cooking'];
+
+// Objects
+
+
