355d5b7f50af1cd2a6315818b8631b6af4da2dc6
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-10 02:34:09 +0600

    Add string manipulation examples in Day_5 JavaScript file

---
+++ JavaScripts/Day_5/string.js
@@ -0,0 +1,34 @@
+const country = 'Bangladesh';
+const city = "Chittagong";
+const location = `Dhaka`;
+
+const tana = new String('Sitakund');
+
+console.log(country);
+console.log(typeof country);
+console.log(city);
+console.log(typeof city);
+console.log(location);
+console.log(typeof location);
+
+
+console.log(tana);
+console.log(typeof tana);
+
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(number);
+console.log(typeof number);
+console.log(number.length);
+
+console.log(number[0]);
+
+
+const capital = 'Dhaka';
+console.log(capital.length);
+console.log(capital[4]);
+
+capital[1] = 'a';
+
+console.log(capital);
