5413f9ecda0465152d88a9b5fc725b46a392844c
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-10 04:09:38 +0600

    Add slice.js file with examples of string slicing and splitting in Day_5 JavaScript directory

---
+++ JavaScripts/Day_5/slice.js
@@ -0,0 +1,12 @@
+const address = 'Chittagong';
+
+const part = address.slice(0, 5);
+console.log(part); // Chitt
+
+
+const sentence = 'I am learning JavaScript';
+
+console.log(sentence.split(' ')); // [ 'I', 'am', 'learning', 'JavaScript' ]
+
+console.log(typeof sentence.split(' ')); // string
+typeof sentence.split(' '); // object
