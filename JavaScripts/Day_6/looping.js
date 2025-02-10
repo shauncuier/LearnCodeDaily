b4edc82f1059f0f5605da25ba5e0a3dc6ec39549
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-11 02:24:33 +0600

    Add looping.js file with examples of iterating over an array of friends

---
+++ JavaScripts/Day_6/looping.js
@@ -0,0 +1,13 @@
+const friends = ['sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid', 'sajjad', 'karim', 'rahim', 'kamal', 'jamal', 'salam', 'sajib', 'sakib', 'sajal', 'sajid'];
+
+console.log(friends.length);
+for (const friend of friends) {
+    // console.log(friend.length);
+}
+
+let names = [];
+
+for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    // console.log(friend);
+}
