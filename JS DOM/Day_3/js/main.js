a825efba27133bee9b2d7484115ab33cd4487151
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-25 16:00:47 +0600

    Add product input fields and button with event listener in main.js and index.html

---
+++ JS DOM/Day_3/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("addProductBtn").addEventListener("click", function () {
+    const productName = document.getElementById("productName").value;
+    const productImage = document.getElementById("productName").value;
+    const productPrice = document.getElementById("productName").value;
+
+    document.getElementById("productList");
+});
