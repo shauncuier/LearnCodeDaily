e4b9a7bffbfb067efb91839e98f4e893b68dd827
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-25 16:30:35 +0600

    Add product addition functionality with dynamic display in index.html and main.js

--- JS DOM/Day_3/js/main.js
+++ JS DOM/Day_3/js/main.js
@@ -1,7 +1,30 @@
 document.getElementById("addProductBtn").addEventListener("click", function () {
     const productName = document.getElementById("productName").value;
-    const productImage = document.getElementById("productName").value;
-    const productPrice = document.getElementById("productName").value;
+    const productImage = document.getElementById("productImage").value;
+    const productPrice = document.getElementById("productPrice").value;
 
-    document.getElementById("productList");
+    const productCard = document.getElementById("productList");
+
+    const newProduct = document.createElement("div");
+    newProduct.innerHTML = `
+
+        <div class="card bg-base-100 w-[20%] shadow-sm">
+                <figure>
+                    <img src="${productImage}" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                    ${productName}
+                        <div class="badge badge-secondary">Name</div>
+                    </h2>
+
+                    <div class="card-actions justify-end">
+
+                        <div class="badge badge-outline">Price: $${productPrice}</div>
+                    </div>
+                </div>
+            </div>
+
+    `;
+    productCard.appendChild(newProduct);
 });
