81bb258cf30afea25767a4851b05b66fad09b751
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-05 00:32:14 +0600

    Add spread operator examples and explanations in spread.js

---
+++ ES6/arrowFuntion/spread.js
@@ -0,0 +1,33 @@
+//  Spread operator
+//  Spread operator is used to split up array elements or object properties.
+//  Spread operator is used to copy array elements or object properties.
+//  Spread operator is used to merge array elements or object properties.
+//  Spread operator is used to pass elements of an array as arguments to a function.
+//  Spread operator is used to copy an array.
+//  Spread operator is used to merge arrays.
+//  Spread operator is used to merge objects.
+//  Spread operator is used to copy objects.
+//  Spread operator is used to add new elements to an array.
+//  Spread operator is used to add new properties to an object.
+//  Spread operator is used to add new elements to an array.
+//  Spread operator is used to add new properties to an object.
+//  Spread operator is used to add new elements to an array.
+//  Spread operator is used to add new properties to an object.
+//  Spread operator is used to add new elements to an array.
+
+// 1. Spread operator with array
+
+// Syntax: [...array]
+
+// Example:
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(...numbers);
+
+// 2. Spread operator with object
+
+
+
+
+
