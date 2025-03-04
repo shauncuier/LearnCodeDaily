97323c40a96887b2f6e5c68ab2a9bce33534c83a
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-05 00:49:02 +0600

    Refactor destructuring example to use an array instead of an object

--- ES6/arrowFuntion/distructure.js
+++ ES6/arrowFuntion/distructure.js
@@ -1,22 +1,29 @@
-const person = {
-    name: 'John',
-    age: 25,
-    address: {
-        city: 'New York',
-        country: 'USA'
-    }
+// const person = {
+//     name: 'John',
+//     age: 25,
+//     address: {
+//         city: 'New York',
+//         country: 'USA'
+//     }
 
-};
+// };
 
-const { name, age, address: { city, country } } = person;
+// const { name, age, address: { city, country } } = person;
 
+
+// console.log(name); // John
+// console.log(age); // 25
+// console.log(city); // New York
+// console.log(country); // USA
+
+
+
+const person = ['John', '25', 'New York'];
+const [name, age, city] = person;
 
 console.log(name); // John
-console.log(age); // 25
+console.log(age); // 25 
 console.log(city); // New York
-console.log(country); // USA
-
-
 
 
 
