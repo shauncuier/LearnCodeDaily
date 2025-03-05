ba4fc91fc05b78f60649e5882ddd693a1371f6d5
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-06 05:56:30 +0600

    Add mobile products array and examples of filtering and mapping

--- ES6/Day_2/index.js
+++ ES6/Day_2/index.js
@@ -33,16 +33,16 @@
 // console.log(person.details.parents.father);
 
 const products = [
-    {id: 1, name: 'laptop', price: 1000},
-    {id: 2, name: 'desktop', price: 1500},
-    {id: 3, name: 'phone', price: 500},
-    {id: 4, name: 'tablet', price: 800},
-    {id: 5, name: 'smartwatch', price: 400},
-    {id: 6, name: 'keyboard', price: 50},
-    {id: 7, name: 'mouse', price: 30},
-    {id: 8, name: 'monitor', price: 300},
-    {id: 9, name: 'printer', price: 200},
-    {id: 10, name: 'speaker', price: 150},
+    { id: 1, name: 'laptop', price: 1000 },
+    { id: 2, name: 'desktop', price: 1500 },
+    { id: 3, name: 'phone', price: 500 },
+    { id: 4, name: 'tablet', price: 800 },
+    { id: 5, name: 'smartwatch', price: 400 },
+    { id: 6, name: 'keyboard', price: 50 },
+    { id: 7, name: 'mouse', price: 30 },
+    { id: 8, name: 'monitor', price: 300 },
+    { id: 9, name: 'printer', price: 200 },
+    { id: 10, name: 'speaker', price: 150 },
 
 
 ];
@@ -63,7 +63,7 @@ const newProduct = products.filter(product => product.price >= 1000);
 products.forEach(product => {
     if (product.price > 500) {
         // console.log(product.name);
-        
+
     }
 });
 
@@ -73,5 +73,46 @@ products.forEach(product => {
 
 const productNames = products.map(product => product.name);
 // console.log(productNames);
+
+
+
+const mobileProducts = [
+    { id: 1, name: 'iphone', price: 1000, color: 'black' },
+    { id: 2, name: 'samsung', price: 800, color: 'white' },
+    { id: 3, name: 'nokia', price: 500, color: 'red' },
+    { id: 33, name: 'nokia', price: 500, color: 'red' },
+    { id: 23, name: 'nokia', price: 500, color: 'red' },
+    { id: 44, name: 'huawei', price: 600, color: 'blue' },
+    { id: 5, name: 'sony', price: 700, color: 'green' },
+    { id: 6, name: 'lg', price: 400, color: 'yellow' },
+    { id: 7, name: 'htc', price: 300, color: 'purple' },
+    { id: 8, name: 'motorola', price: 200, color: 'orange' },
+    { id: 9, name: 'blackberry', price: 150, color: 'pink' },
+    { id: 10, name: 'alcatel', price: 100, color: 'brown' },
+];
+
+
+const singleMobileProduct = mobileProducts.filter(product => product.name === 'nokia');
+console.log(singleMobileProduct);
+
+
+const singleProduct = mobileProducts.find(product => product.id === 8);
+// console.log(singleProduct);
+
+
+mobileProducts.forEach(product => {
+    // console.log(product);
+});
+
+const newMobileProducts = mobileProducts.map(product => {
+    return {
+        ...product,
+        price: product.price + 100,
+        color: product.color.toUpperCase(),
+    }
+});
+
+// console.log(newMobileProducts);
+
 
 
