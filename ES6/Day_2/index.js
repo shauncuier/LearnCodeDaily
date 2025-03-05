70fd5a3ecf7e619ea6d061f248e6f3a5c4c5b836
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-06 01:02:23 +0600

    Migrate arrow function and destructuring examples to new files; remove old implementations

---
+++ ES6/Day_2/index.js
@@ -0,0 +1,14 @@
+
+const person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['reading', 'cooking', 'swimming'],
+};
+
+console.log(person.name);
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
+
+
+
