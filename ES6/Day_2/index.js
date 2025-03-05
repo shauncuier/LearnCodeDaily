6f007fd7244aced29cba01c87a5aef74a8b2c3b3
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-06 04:47:29 +0600

    Update index.html and index.js: comment out script tag and expand person object with additional details

--- ES6/Day_2/index.js
+++ ES6/Day_2/index.js
@@ -1,14 +1,37 @@
 
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     hobbies: ['reading', 'cooking', 'swimming'],
+// };
+
+// console.log(person.name);
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+
 const person = {
     name: 'John',
     age: 30,
-    hobbies: ['reading', 'cooking', 'swimming'],
-};
+    hobbies: ['reading', 'cooking', 'swimming', 'coding', 'gaming', 'watching movies'],
+    details: {
+        address: '123 Main Street',
+        city: 'New York',
+        isMarried: false,
+        hasKids: true,
+        job: 'Developer',
+        salary: 100000,
+        parents:{
+            father: 'Mike',
+            mother: 'Jane'
+        }
+    },
+}
 
-console.log(person.name);
-console.log(Object.keys(person));
-console.log(Object.values(person));
-console.log(Object.entries(person));
+// console.log(person.details.address);
+// console.log(person.details.parents.father);
+
 
 
 
