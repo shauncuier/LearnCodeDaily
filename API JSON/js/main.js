47ea3c2a14200fe3b127d6734bacecae958b8b62
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-10 01:14:59 +0600

    Add initial HTML and JavaScript for API JSON test; implement data loading functionality

---
+++ API JSON/js/main.js
@@ -0,0 +1,34 @@
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+
+
+// const loadData = () => {
+//     fetch('https://jsonplaceholder.typicode.com/todos')
+//         .then(response => response.json())
+//         .then(json => console.log(json))
+// }
+
+
+// console.log('Hello World!')
+// loadData();
+
+// show all data in browser
+
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+            const ul = document.getElementById('data');
+            json.forEach(item => {
+                const li = document.createElement('li');
+                li.innerText = item.title;
+                ul.appendChild(li);
+            })
+        })
+}
+
+
+console.log(json);
+
