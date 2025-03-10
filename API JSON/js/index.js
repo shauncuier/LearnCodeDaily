b04216248f68972a57ec320369f6a0263ebcb18d
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-11 02:18:11 +0600

    Update user display to show company name and clean up script references in index.html

--- API JSON/js/index.js
+++ API JSON/js/index.js
@@ -1,20 +1,57 @@
-const loadUser = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+// const loadUser = () => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         displayUser(data);
+//     }
+//     );
+// }
+
+// const displayUser = (users) => {
+//     const ul = document.getElementById('data');
+//     for (const user of users) {
+//         const li = document.createElement('li');
+//         li.innerText = user.name;
+//         ul.appendChild(li);
+//     }
+// }
+
+
+
+
+// setTimeout(() => {
+//     console.log('I am from first line');
+// }, 3);
+
+// setTimeout(() => {
+//     console.log('I am from second line');
+// }, 2);
+
+// setTimeout(() => {
+//     console.log('I am from setTimeout');
+// }, 1);
+
+
+
+fetch('./json/test.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
         displayUser(data);
     }
     );
-}
 
 const displayUser = (users) => {
     const ul = document.getElementById('data');
     for (const user of users) {
         const li = document.createElement('li');
-        li.innerText = user.name;
+        li.innerText = user.company;
         ul.appendChild(li);
     }
 }
+
+
+
 
 
