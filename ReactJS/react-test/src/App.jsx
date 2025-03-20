5eb50d05799a7cc70b39522c869372da2f3175d4
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-20 22:42:15 +0600

    Fix capitalization of "Count" in button label in App component

--- ReactJS/react-test/src/App.jsx
+++ ReactJS/react-test/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Hello World!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
         <p>Count</p>
       </div>
     </>
   );
 }
-
 export default App;
