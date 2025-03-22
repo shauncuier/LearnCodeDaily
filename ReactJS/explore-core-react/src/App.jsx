82b718b4a20c0609e772e7ecf8737955785ff0ef
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-23 02:20:26 +0600

    Add Person, Products, and Button components to enhance App structure

--- ReactJS/explore-core-react/src/App.jsx
+++ ReactJS/explore-core-react/src/App.jsx
@@ -2,13 +2,41 @@
 import './App.css'
 
 function App() {
- 
-
   return (
     <>    
       <h1>Explore Core React</h1>
-      
+      <h2>Hi</h2>
+      <Person />
+      <Products />
+      <Button />
     </>
+  )
+}
+
+function Person() {
+  const name = 'John Doe';
+  const age = 25;
+  return (
+    <p>I am a {name} age = {age}</p>
+  )
+}
+
+function Products() {
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  )
+}
+
+function Button() {
+  return (
+    <button>Click Me</button>
   )
 }
 
