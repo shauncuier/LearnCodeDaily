a1fbdcbf0000c19d0a188f56dbc83f8f4e4efeca
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-25 01:59:57 +0600

    Add New component to display introductory content

---
+++ ReactJS/explore-core-react/src/components/New.jsx
@@ -0,0 +1,12 @@
+
+
+const New = () => {
+    return (    
+        <div>
+            <h1 className="text-2xl">New</h1>
+            <p>Some new content</p>
+        </div>
+    );
+}
+
+export default New;
