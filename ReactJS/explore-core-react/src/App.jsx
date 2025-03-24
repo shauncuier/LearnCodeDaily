a40601adf5d417e4480c4b0c89c7ebda5ad136b4
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-25 00:06:43 +0600

    Integrate Tailwind CSS for improved styling and update App component structure

--- ReactJS/explore-core-react/src/App.jsx
+++ ReactJS/explore-core-react/src/App.jsx
@@ -3,58 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <>    
-      <h1>Explore Core React</h1>
-      <h2>Hi</h2>
-      <Person />
-      <Products />
-      <Button />
-
-      <Developer name="John Doe" tech="React" />
-      <Developer name="Jane Doe" tech="Angular" />
-      <Developer name="Alice" tech="Vue" />
-      <Developer name="Bob" tech="Svelte" />
-      <Developer name="Charlie" tech="Ember" />
-      
-
+    <>   
+      <h1 className='text-red-300 text-2xl'>hello</h1>
     </>
   )
 }
 
-function Developer(props) {
-  return (
-    <div className='button' style={{border: '2px solid #646cffaa', margin: '10px', padding: '10px', borderRadius: '15px'}}>
-      <h3>Developer: {props.name}</h3>
-      <p> Tecnology: {props.tech}</p>
-    </div>
-  )
-}
-
-function Person() {
-  const name = 'John Doe';
-  const age = 25;
-  return (
-    <p>I am a {name} age = {age}</p>
-  )
-}
-
-function Products() {
-  return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
-    </div>
-  )
-}
-
-function Button() {
-  return (
-    <button>Click Me</button>
-  )
-}
 
 export default App
