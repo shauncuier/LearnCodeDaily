330e7244c33ee5eea470dfd8dfc8579d3a3bd53b
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-25 00:17:44 +0600

    Refactor Age component styling for improved layout and center alignment

--- ReactJS/explore-core-react/src/components/Age.jsx
+++ ReactJS/explore-core-react/src/components/Age.jsx
@@ -2,7 +2,7 @@
 
 function Age() {
     return (
-        <div className="border-2 border-red-300 p-4 rounded-lg">
+        <div className="border-2 border-red-300 p-4 rounded-lg text-center">
             <h1 className="text-2xl">Age</h1>
             <h1>Age: 25</h1>
         </div>
