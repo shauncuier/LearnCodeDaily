d8e386b3d9b67a91e66f62c31c9e4a1102f23d1f
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-04-15 01:36:42 +0600

    Refactor router setup to improve structure and add about page

--- ReactJS/my-react-router-app/src/main.jsx
+++ ReactJS/my-react-router-app/src/main.jsx
@@ -2,20 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <div><h1>Welcome to my website</h1><p>This is a simple React application.</p></div>,
+    },
+    {
+      path: '/about',
+      element: <div><h1>About</h1><p>This is the about page.</p></div>,
+    }
+  ]
+)
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
