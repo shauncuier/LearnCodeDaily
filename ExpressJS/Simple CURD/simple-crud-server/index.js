172e4b8af4a6d127a14500170bbbbf23bc6958ec
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-05-19 01:57:34 +0600

    Initialize Express server with basic route and add dotenv dependency

---
+++ ExpressJS/Simple CURD/simple-crud-server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Simple CRUD app run on http://localhost:${port}`)
+})
