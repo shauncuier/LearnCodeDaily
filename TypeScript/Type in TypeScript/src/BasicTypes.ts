63a8aea8481b532cc47ae2c5dad31cc8913f931e
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-09-05 02:46:28 +0600

    refactor: Organize BasicTypes.ts by removing unused variables and improving structure

--- TypeScript/Type in TypeScript/src/BasicTypes.ts
+++ TypeScript/Type in TypeScript/src/BasicTypes.ts
@@ -1,8 +1,29 @@
+// Basic Types in TypeScript
+
+
+// Primitive Types
 let name: string = "John Doe";
 let age: number = 30;
 let isStudent: boolean = true;
-
 let notSure: any = "Maybe a string";
-notSure = 42;
-notSure = true;
-let list: number[] = [1, 2, 3, 4, 5];
+
+
+
+// Array Types
+let list: (number)[] = [1, 2, 3, 4, 5];
+let string: Array<string> = ["1"];
+let mixed: (number | string | boolean)[] = [1, "2", 3, true];
+
+
+// Object Types
+let user: {
+    name: string;
+    age: number;
+    gender?: 'male' | 'female' | 'other';
+} = {
+    name: "John Doe",
+    age: 30,
+    gender: 'other'
+};
+
+export { };
