ae02bf5f277904a0464837f484cf4db452e05eb0
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-09-05 02:30:25 +0600

    refactor: Clean up BasicTypes.ts by removing unnecessary empty lines

--- TypeScript/Type in TypeScript/src/BasicTypes.ts
+++ TypeScript/Type in TypeScript/src/BasicTypes.ts
@@ -1,5 +1,8 @@
+let name: string = "John Doe";
+let age: number = 30;
+let isStudent: boolean = true;
 
-
-
-
-
+let notSure: any = "Maybe a string";
+notSure = 42;
+notSure = true;
+let list: number[] = [1, 2, 3, 4, 5];
